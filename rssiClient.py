6d514fcc9da9b0751ea0254658c3ce0058af8ac2
arjun <f.cendana@gmail.com>
2022-12-30 21:52:37 -0500

    Updated from client end

--- rssiClient.py
+++ rssiClient.py
@@ -9,7 +9,7 @@ btAddr = '34:43:0b:0b:bd:9b'
 bufferSize = 1024
 
 #cmd = 'hcitool rssi ' + btAddr
-cmd = 'sudo iw wlan0 scan | grep "SSID: Nucleus" -B2'
+command = 'sudo iw wlan0 scan | grep "SSID: Nucleus" -B2'
 
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 server.connect((addr, port))
@@ -25,7 +25,8 @@ while True:
         break
     elif cmd == 'u':
         res = 'u'
-        res += os.popen(cmd).read()[-3:-1]
+        #res += os.popen(command).read()[-3:-1]
+        res += os.popen(command).read()[8:14]
         server.send(bytes(res, 'utf-8'))
     else:
         server.send(bytes(res,'utf-8'))
